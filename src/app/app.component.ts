9716aaf8e618195ce44edbfa70c029ca4e079633
Roberto Valenzuela <robval.1721@gmail.com>
2020-05-28 16:31:21 -0400

    Intento de subir platzi-store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store';
+  agregarItem = 'Add item';
+  items = ['nicolas', 'julian', 'roberto']; // array a recorrer
+
+  addItem() {
+    this.items.push(this.agregarItem);
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1); // splice (posicion , cuantos a eliminar)
+  }
 }
