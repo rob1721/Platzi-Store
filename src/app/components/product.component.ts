a5e034c288d0869b664fe49a1eb6a785655b4dd1
Roberto Valenzuela <robval.1721@gmail.com>
2020-05-29 23:54:56 -0400

    agregado cuadros para separar productos, probado ng g c

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -24,6 +24,8 @@ export class ProductComponent implements OnInit, DoCheck, OnDestroy{
     @Output() productClicked: EventEmitter<any> = new EventEmitter();  // eventemitter necesita valor inicial
     // tslint:disable-next-line: no-trailing-whitespace
 
+    today = new Date();
+
     constructor(){
       console.log('1. constructor');
     }
