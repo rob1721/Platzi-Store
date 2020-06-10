830040f56ce3b074e8fb313476f16ee1514dff33
Roberto Valenzuela <robval.1721@gmail.com>
2020-06-10 15:14:18 -0400

    refactor(product, cart): producto y cart modularizados

---
+++ src/app/product/product.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductComponent } from './components/product/product.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+
+import { ProductsRoutingModule } from './product-routing.module';
+import { SharedModule } from './../shared/shared.module';
+
+@NgModule({
+    declarations: [
+        ProductsComponent,
+        ProductDetailComponent,
+        ProductComponent
+    ],
+    imports: [
+        CommonModule,
+        SharedModule,
+        ProductsRoutingModule
+    ],
+    exports: [
+
+    ]
+})
+
+export class ProductModule { }
