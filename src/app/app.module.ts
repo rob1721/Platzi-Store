a3d3d8c5ed4aa6f6282a8ded81993718e48151c9
Roberto Valenzuela <robval.1721@gmail.com>
2020-07-15 03:04:03 -0400

    lot of things [old]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CartComponent } from './shared/components/cart/cart.component';
-import { ContactComponent } from './contact/contact.component';
-import { DemoComponent } from './demo/demo.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LayoutComponent } from './layout/layout.component';
 
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './demo/components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CartComponent,
-    ContactComponent,
-    DemoComponent,
-    PageNotFoundComponent,
     LayoutComponent,
-    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +22,8 @@ import { NavbarComponent } from './demo/components/navbar/navbar.component';
     FormsModule,
     SharedModule,
     CoreModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
